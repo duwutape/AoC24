ae05f6966e3d14fe3e3d6aac361ee22985115f7b
duwutape <raven.langheld@gmx.de>
2024-12-02 20:18:55 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import day01.Day01;
+
+public class Main {
+    public static void main(String[] args) {
+        Day01 day01 = new Day01();
+    }
+}
---
+++ src/day01/Day01.java
@@ -0,0 +1,58 @@
+package day01;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Day01 {
+    public Day01() {
+        try {
+            solve();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void solve() throws FileNotFoundException {
+        File input = new File("src/day01/input.txt");
+            Scanner scanner = new Scanner(input);
+
+        ArrayList<Integer> left = new ArrayList<>();
+        ArrayList<Integer> right = new ArrayList<>();
+
+        while (scanner.hasNextLine()){
+            String line =  scanner.nextLine();
+            String[] data = line.split(" {3}");
+            left.add(Integer.parseInt(data[0]));
+            right.add(Integer.parseInt(data[1]));
+
+        }
+
+        Collections.sort(left);
+        Collections.sort(right);
+
+        int sum_abs = 0;
+        int sum_sim = 0;
+        for (int numLeft : left) {
+            sum_abs += Math.abs((numLeft - right.get(left.indexOf(numLeft))));
+
+            int count = 0;
+            for (int numRight : right){
+                if (numLeft == numRight){
+                    count ++;
+                }
+            }
+            sum_sim += numLeft * count;
+        }
+
+        System.out.println(sum_abs);
+        System.out.println(sum_sim);
+
+    }
+}
+
+
+
