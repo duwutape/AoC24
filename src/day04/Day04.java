be50030028517ef1515dfd999e5003e3c630c863
duwutape <raven.langheld@gmx.de>
2024-12-04 18:21:43 +0100

    solved day 4

--- src/day04/Day04.java
+++ src/day04/Day04.java
@@ -7,7 +7,8 @@ import java.util.Scanner;
 
 public class Day04 {
 
-    private int count;
+    private int count1 = 0;
+    private int count2 = 0;
 
     public Day04() {
         try {
@@ -24,51 +25,76 @@ public class Day04 {
         ArrayList<char[]> data = new ArrayList<>();
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
-            char[] chars = new char[line.length() - 1];
-            for (int i = 0; i < line.length() - 1; i++) {
+            char[] chars = new char[line.length()];
+            for (int i = 0; i < line.length(); i++) {
                 chars[i] = line.charAt(i);
             }
             data.add(chars);
         }
-        count = 0;
-        seek(data);
 
-        System.out.println(count);
+        seek1(data);
+
+        System.out.println(count1);
+        System.out.println(count2);
     }
 
-    private void seek(ArrayList<char[]> data) {
+    private void seek1(ArrayList<char[]> data) {
         for (char[] chars : data) {
-            for (int i = 0; i < chars.length; i++) {
-                if (chars[i] == 'X') {
-                    int x = i;
+            for (int x = 0; x < chars.length; x++) {
+                if (chars[x] == 'X') {
                     int y = data.indexOf(chars);
-                    seek(data, x, y, 'X');
+                    seek1(data, x, y, 'X');
+                } else if (chars[x] == 'A') {
+                    int y = data.indexOf(chars);
+                    seek1(data, x, y, 'A');
                 }
             }
         }
     }
 
-    private void seek(ArrayList<char[]> data, int x, int y, char currentChar) {
-        for (int i = -1; i <= 1; i++) {
-            if (y + i >= 0 && y + i <= data.size()-1) {
-                for (int j = -1; j <= 1; j++) {
-                    char[] chars = data.get(y + i);
-                    if (x + j>= 0 && x+ j <= chars.length - 1) {
-                        if (currentChar == 'X' && chars[x + j] == 'M') {
-                            int newX = x+j;
-                            int newY = y+i;
+    private void seek1(ArrayList<char[]> data, int x, int y, char currentChar) {
+        if (currentChar == 'X') {
+            for (int i = -1; i <= 1; i++) {
+                if (inBounds(y + i, data)) {
+                    for (int j = -1; j <= 1; j++) {
+                        char[] chars = data.get(y + i);
+                        if (inBounds(x + j, chars)) {
+                            if (chars[x + j] == 'M') {
+                                int newX = x + j;
+                                int newY = y + i;
 
-                            if ()
-                            seek(data, x + j, y + i, 'M');
-                        } else if (currentChar == 'M' && chars[x + j] == 'A') {
-                            seek(data, x + j, y + i, 'A');
-                        } else if (currentChar == 'A' && chars[x + j] == 'S') {
-                            count++;
+                                int xMod = newX - x;
+                                int yMod = newY - y;
+
+                                if (inBounds(newX + xMod, chars) && inBounds(newX + 2 * xMod, chars) && inBounds(newY + yMod, data) && inBounds(newY + 2 * yMod, data)) {
+                                    if (data.get(newY + yMod)[newX + xMod] == 'A' && data.get(newY + 2 * yMod)[newX + 2 * xMod] == 'S') {
+                                        count1++;
+                                    }
+                                }
+                            }
                         }
                     }
                 }
             }
+        } else {
+            if (inBounds(x - 1, data.get(y)) && inBounds(x + 1, data.get(y)) && inBounds(y - 1, data) && inBounds(y + 1, data)) {
+                if (((data.get(y - 1)[x - 1] == 'M' && data.get(y + 1)[x + 1] == 'S') || (data.get(y - 1)[x - 1] == 'S' && data.get(y + 1)[x + 1] == 'M')) &&
+                        ((data.get(y - 1)[x + 1] == 'M' && data.get(y + 1)[x - 1] == 'S') || (data.get(y - 1)[x + 1] == 'S' && data.get(y + 1)[x - 1] == 'M'))) {
+                    count2++;
+                }
+            }
         }
     }
-}
 
+    private boolean inBounds(int index, char[] chars) {
+        if (index >= 0 && index < chars.length) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean inBounds(int index, ArrayList<char[]> data) {
+        return index >= 0 && index < data.size();
+    }
+}
