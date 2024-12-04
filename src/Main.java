b6fb1305cb7b21fa23a39eb9653dadeb5fc386a9
duwutape <raven.langheld@gmx.de>
2024-12-04 12:23:07 +0100

    started day 4

--- src/Main.java
+++ src/Main.java
@@ -28,7 +28,7 @@ public class Main {
     public static void main(String[] args) {
         //new Day01();
         //new Day02();
-        new Day03();
+        //new Day03();
         new Day04();
         new Day05();
         new Day06();
--- src/day04/Day04.java
+++ src/day04/Day04.java
@@ -1,8 +1,14 @@
 package day04;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Day04 {
+
+    private int count;
+
     public Day04() {
         try {
             solve();
@@ -12,6 +18,57 @@ public class Day04 {
     }
 
     private void solve() throws FileNotFoundException {
+        File input = new File("src/day04/input.txt");
+        Scanner scanner = new Scanner(input);
 
+        ArrayList<char[]> data = new ArrayList<>();
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            char[] chars = new char[line.length() - 1];
+            for (int i = 0; i < line.length() - 1; i++) {
+                chars[i] = line.charAt(i);
+            }
+            data.add(chars);
+        }
+        count = 0;
+        seek(data);
+
+        System.out.println(count);
+    }
+
+    private void seek(ArrayList<char[]> data) {
+        for (char[] chars : data) {
+            for (int i = 0; i < chars.length; i++) {
+                if (chars[i] == 'X') {
+                    int x = i;
+                    int y = data.indexOf(chars);
+                    seek(data, x, y, 'X');
+                }
+            }
+        }
+    }
+
+    private void seek(ArrayList<char[]> data, int x, int y, char currentChar) {
+        for (int i = -1; i <= 1; i++) {
+            if (y + i >= 0 && y + i <= data.size()-1) {
+                for (int j = -1; j <= 1; j++) {
+                    char[] chars = data.get(y + i);
+                    if (x + j>= 0 && x+ j <= chars.length - 1) {
+                        if (currentChar == 'X' && chars[x + j] == 'M') {
+                            int newX = x+j;
+                            int newY = y+i;
+
+                            if ()
+                            seek(data, x + j, y + i, 'M');
+                        } else if (currentChar == 'M' && chars[x + j] == 'A') {
+                            seek(data, x + j, y + i, 'A');
+                        } else if (currentChar == 'A' && chars[x + j] == 'S') {
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
     }
 }
+
